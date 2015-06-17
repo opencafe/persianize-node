26484ba8787622ae1bc94a6793cc608741520f89
ShahrokhNiakan <shahrokhniakan15@gmail.com>
2015-06-17 15:31:48 +0430

    update validator

--- validator.js
+++ validator.js
@@ -1,5 +1,10 @@
-exports.number = function() {
-	
-		return 'number';
+exports.number = function(num) {
 
+		  if (isNaN(num)){
+		    console.log("is not valid");
+		  }
+		  else {
+		    console.log("is valid");
+			  return num;
+		  }
 }
