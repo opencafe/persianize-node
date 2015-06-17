4a3d2dd7f388f20725787c62b422e3e1e160e559
ShahrokhNiakan <shahrokhniakan15@gmail.com>
2015-06-17 15:24:28 +0430

    update convert.js

--- convert.js
+++ convert.js
@@ -1,5 +1,12 @@
-exports.number = function() {
-	
-	return 'number';
-
+exports.number = function(num) {
+	var per_num = [
+    '۰','۱','۲','۳','۴',
+    '۵','۶','۷','۸','۹'
+  ];
+  var str_num = "";
+  //num = String(num);
+  for (var i = 0; i < num.length; i++) {
+    str_num += per_num[parseInt(num.charAt(i))];
+  }
+  return str_num;
 }
