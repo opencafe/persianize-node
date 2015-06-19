a3cecef25cc7f3e08de9144792218e0e9326d2e9
ShahrokhNiakan <shahrokhniakan15@gmail.com>
2015-06-20 01:04:19 +0430

    update convert.js

--- convert.js
+++ convert.js
@@ -1,10 +1,11 @@
+
 exports.convert = function(num) {
 	var per_num = [
     "\xDB\xB0", "\xDB\xB1", "\xDB\xB2", "\xDB\xB3", "\xdb\xb4",
     "\xDB\xB5", "\xDB\xB6", "\xDB\xB7", "\xDB\xB8", "\xDB\xB9"
   ];
   var persian_num = "";
-  num = String(num);
+  //num = String(num);
   for (var i = 0; i < num.length; i++) {
     persian_num += (parseInt(per_num[parseInt(num.charAt(i))], 16)).toString();
   }
