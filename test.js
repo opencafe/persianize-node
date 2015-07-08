9182c9e8fcf08090f5f17e9f600bee283a3fdfef
ShahrokhNiakan <shahrokhniakan15@gmail.com>
2015-07-08 20:38:10 +0430

    creating node package

--- test.js
+++ test.js
@@ -8,8 +8,8 @@ console.log( "This Experation is persian alpha_num {persian}: --------------- " 
 console.log( "This Experation is persian alpha_num {۳فارسی}: --------------- " + persianize.validator().alpha_num('۳فارسی'));
 console.log( "This Experation is persian text {persian}: --------------- " +  persianize.validator().text('persian') );
 console.log( "This Experation is persian text {۳ فارسی}: --------------- " + persianize.validator().text('۳ فارسی'));
-console.log("Convert english number to persian number: --------------- " + persianize.convert().number("1234").get());
-console.log("Convert Arabic charachters to persian: --------------- " + persianize.convert().removeArabicChar("می روي‌").get());
-console.log("Replace half-space to persian verbs: -------------- " + persianize.convert().half_space("می روی‌").get());
-console.log("Replace half-space to persian verbs: -------------- " + persianize.convert().half_space("آمده ای").get());
-console.log("Convert All Arabic charachters to persian: --------------- " + persianize.convert().all("می روي‌").get());
+console.log( "Convert english number to persian number: --------------- " + persianize.convert().number("1234").get());
+console.log( "Convert arabic charachters to persian: --------------- " + persianize.convert().removeArabicChar("می روي‌").get());
+console.log( "Replace half-space to persian verbs: -------------- " + persianize.convert().half_space("می روی‌").get());
+console.log( "Replace half-space to persian verbs: -------------- " + persianize.convert().half_space("آمده ای").get());
+console.log( "Convert all options to persian: --------------- " + persianize.convert().all("12 می روي‌").get());
